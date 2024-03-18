f43ddb23a8384928a20b35ea27e476ee5b83430c
Mark Manos <mark@airpac.com>
2024-03-17 22:01:27 -0600

    Type updates for ajax, resource, and core.

--- src/ajax.d.ts
+++ src/ajax.d.ts
@@ -1,7 +1,19 @@
 import type { PromiseInterface } from "@mmanos/swing-js/src/promise";
 
+export interface AjaxOptions {
+	url: string;
+	type?: 'GET'|'POST'|'PUT'|'DELETE'|'OPTIONS';
+	headers?: { [key: string]: any };
+	data?: { [key: string]: any };
+	raw?: any;
+	files?: { [key: string]: any };
+	json?: { [key: string]: any };
+	onprogress?: Function;
+	[key: string]: any;
+};
+
 export interface Ajax {
-	(options: object): PromiseInterface;
+	(options: AjaxOptions): PromiseInterface;
 	precheck(options: object): object;
 	postcheck(options: object, d: PromiseInterface): void;
 	_makeRequest(options: object): PromiseInterface;
--- src/resource.d.ts
+++ src/resource.d.ts
@@ -1,11 +1,37 @@
+import type { AjaxOptions } from "./ajax";
+import type { PromiseInterface } from "./promise";
+
 export interface ConstructableResource<T> {
 	new(...args: any): T;
 }
 
+export interface ResourceOptions extends AjaxOptions {
+	url?: string;
+}
+
+export interface ResourceSaveOptions extends ResourceOptions {
+	extra_data?: object;
+}
+
 export interface Resource {
 	$urlRoot: string;
+	$set: (key: string|object, val?: any) => void;
+	$url: () => string;
+	$fetch: (options?: ResourceOptions) => PromiseInterface;
+	$save: (options?: ResourceSaveOptions) => PromiseInterface;
+	$destroy: (options?: ResourceOptions) => PromiseInterface;
+	$restore: () => void;
+	$load: (data: Resource|object) => void;
+	$toJSON: () => object;
+	$hasChanged: () => boolean;
+	$changed: () => object;
+	$replicate: () => Resource;
+	$clone: () => Resource;
+	query(options?: ResourceOptions): PromiseInterface;
+	find(id: number|string, options?: ResourceOptions): PromiseInterface;
+	paginate(page?: number, num?: number, options?: ResourceOptions): PromiseInterface;
+	all(num?: number|object, options?: ResourceOptions): PromiseInterface;
 	extend<T>(protoProps: T, staticProps?: T): T & Resource & ConstructableResource;
-	find(id: number|string): Resource;
-};
+}
 
 export default resource as Resource;
